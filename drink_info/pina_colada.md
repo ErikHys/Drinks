22873c7d55f51187f8c6af49047c7a55fc359e91
ErikHys <E.Hystad@student.uib.no>
2022-06-30 19:26:37 +0200

    pina not working

--- drink_info/pina_colada.md
+++ drink_info/pina_colada.md
@@ -1,6 +1,8 @@
 ---
 title: 'Pina Colada'
 drinkType: 'rum'
-img: '/drink_imgs/temp.jpg'
+img: '/drink_imgs/pina.jpg'
 ---
-?
+If you like pina coladas <br>
+and getting caught in the rain <br>
+... queue "Escape" by Rupert Holmes
--- pages/menus/gin_drinks.js
+++ pages/menus/gin_drinks.js
@@ -86,6 +86,7 @@ export async function getStaticProps() {
         } catch (e) {
             console.error(e)
             console.error("id:" + id)
+            console.error("json[42]" + tempData[42])
         }
         return tempData;
     }))).filter((drinkInfo) => {
